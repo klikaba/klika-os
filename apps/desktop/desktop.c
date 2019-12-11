a3a468444c2cbc0fba752cd1cf42e03d04c89150
Ensar Sarajčić <ensar.sarajcic@klika.ba>
2019-12-11 14:34:23 +0100

    Add support for frame flags to remove frame from desktop (#17)

--- apps/desktop/desktop.c
+++ apps/desktop/desktop.c
@@ -20,14 +20,14 @@ void start_app(char *filename) {
 int main() {
 	bmp_from_file("/apps/desktop/appicon.bmp", &app_icon_bmp);
 	
-	window = window_create(120, 120, 300, 200, "Applications", MSG_USER_WIN, WINDOW_ATTR_BOTTOM | WINDOW_ATTR_NO_DRAG);
-	button_t *btn = button_create(window, 10, WINDOW_BAR_HEIGHT + 10, 64, 64 + 9, "Simple", MSG_USER_BTN_SIMPLE_WIN);
+	window = window_create(0, 0, 1024, 768, "Applications", MSG_USER_WIN, WINDOW_ATTR_BOTTOM | WINDOW_ATTR_NO_DRAG, WINDOW_FRAME_NONE);
+	button_t *btn = button_create(window, 10, 10, 64, 64 + 9, "Simple", MSG_USER_BTN_SIMPLE_WIN);
 	button_set_image(btn, BUTTON_STATE_NORMAL, &app_icon_bmp);
 
-	btn = button_create(window, 90, WINDOW_BAR_HEIGHT + 10, 64, 64 + 9, "Demo1", MSG_USER_BTN_DEMO1);
+	btn = button_create(window, 90, 10, 64, 64 + 9, "Demo1", MSG_USER_BTN_DEMO1);
 	button_set_image(btn, BUTTON_STATE_NORMAL, &app_icon_bmp);
 
-	btn = button_create(window, 170, WINDOW_BAR_HEIGHT + 10, 64, 64 + 9, "Demo2", MSG_USER_BTN_DEMO2);
+	btn = button_create(window, 170, 10, 64, 64 + 9, "Demo2", MSG_USER_BTN_DEMO2);
 	button_set_image(btn, BUTTON_STATE_NORMAL, &app_icon_bmp);
 
 	while(window_get_message(window, &msg)) { 

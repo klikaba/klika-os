6dd546c75e8654faa54a2a7ae0bd2059f3356411
Ensar Sarajčić <ensar.sarajcic@klika.ba>
2019-12-12 01:42:33 +0100

    Clean up comments in file explorer

--- apps/file_explorer/file_explorer.c
+++ apps/file_explorer/file_explorer.c
@@ -14,26 +14,18 @@ window_t  *label;
 
 void load_directory_entries(char* dirname, uint8_t depth) {
 	int layout_y = WINDOW_BAR_HEIGHT + 10;
-	DEBUG("LOOKING FOR FILES IN %s\n", dirname);
 	DIR *dp;
 	DIRENT *ep;     
 	dp = opendir(dirname);
 
 	if (dp != NULL)
 	{
-		DEBUG("OPENED THE DIR");
 		while (ep = readdir (dp)) {
-			DEBUG("FOUND FILE %s\n", ep->name);
 			if (ep->name[0] != '.') {
 				char display_name[64];
 				sprintf(display_name, "%s/%s(%s)", dirname, ep->name, ep->attr & ATTR_DIRECTORY ? "DIR" : "FILE");
 				label = label_create(window, depth * 10, layout_y + 30 * label_counter++, 100, 20, display_name, MSG_USER_LABEL);
 			}
-			/* if (ep->name[0] != '.' && ep->attr & ATTR_DIRECTORY) { */
-			/* 	char full_name[64]; */
-			/* 	sprintf(full_name, "%s/%s", dirname, ep->name); */
-			/* 	load_directory_entries(full_name, depth + 1); */
-			/* } */
 		}
 
 		(void) closedir (dp);

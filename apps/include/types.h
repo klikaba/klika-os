d61e27e42d2a5cfd1a7b470ba0fd2e2dcc9eec9c
Imamovic, Haris <haris.imamovic@bawagpsk.com>
2019-12-13 13:23:38 +0100

    Partial implementation of time.h lib

--- apps/include/types.h
+++ apps/include/types.h
@@ -1,9 +1,14 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
+/**
+ * This macro is the value of a null pointer constant.
+ * */
+#define NULL (void *)0
 
-#define NULL (void*)0
-
+/**
+ * This is the unsigned integral type and is the result of the sizeof keyword.
+ * */
 typedef __SIZE_TYPE__ size_t;
 
 #endif

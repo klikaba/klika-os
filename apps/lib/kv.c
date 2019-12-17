900d7c80ac857926396b8a34103108c250c03aa5
Ensar Sarajčić <ensar.sarajcic@klika.ba>
2019-12-17 11:22:34 +0100

    Implement kv write using file write functionality

--- apps/lib/kv.c
+++ apps/lib/kv.c
@@ -16,6 +16,21 @@ char buf_getc(uint8_t* ptr, int index, int size) {
 	return index >= size ? -1 : ptr[index];
 }
 
+kv_file_t* create_empty_kv() {
+	kv_file_t* kv = malloc(sizeof(kv_file_t));
+	kv->entry_count = 0;
+	kv->keys = malloc(sizeof(char*) * MAX_PAIRS);
+	kv->values = malloc(sizeof(char*) * MAX_PAIRS);
+	return kv;
+}
+
+void add_kv_entry(kv_file_t* kv_file, char* key, char* value) {
+	if (kv_file->entry_count >= MAX_PAIRS) return;
+	kv_file->keys[kv_file->entry_count] = strdup(key);
+	kv_file->values[kv_file->entry_count] = strdup(value);
+	kv_file->entry_count++;
+}
+
 void open_kv_file(char* filename, kv_file_t* kv_file_out) {
 	FILE *file = fopen(filename, "r");
 	if (file == NULL) {
@@ -112,6 +127,33 @@ void open_kv_file(char* filename, kv_file_t* kv_file_out) {
 	free(buffer);
 	free(key_holder);
 	free(value_holder);
+}
+
+void create_kv_file(char* filename, kv_file_t* kv_file_data) {
+	FILE *file = fopen(filename, "w");
+	if (file == NULL) {
+		return;
+	}
+	uint32_t size = 0;
+	int i;
+	for (i = 0; i < kv_file_data->entry_count; i++) {
+		// key=value\n
+		size += strlen(kv_file_data->keys[i]) + 1 + strlen(kv_file_data->values[i]) + 1;
+	}
+	uint8_t *buffer = malloc(sizeof(char) * size + 1);
+	uint8_t *iter = buffer;
+	for (i = 0; i < kv_file_data->entry_count; i++) {
+		char next;
+		char* key = kv_file_data->keys[i];
+		char* value = kv_file_data->values[i];
+		while((next = *key++) != '\0') *iter++ = next;
+		*iter++ = '=';
+		while((next = *value++) != '\0') *iter++ = next;
+		*iter++ = '\n';
+		*iter = '\0';
+	}
+	fwrite(buffer, sizeof(char), size, file);
+	fclose(file);
 }
 
 void close_kv_file(kv_file_t* file) {
